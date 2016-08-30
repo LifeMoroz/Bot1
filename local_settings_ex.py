45bde32e5e142c103ddab4599722f9c45f177443
ruslan.galimov <ruslan.galimov@corp.mail.ru>
2016-08-30 12:44:11 +0300

    Add local settings

---
+++ local_settings_ex.py
@@ -0,0 +1,22 @@
+HEADERS = {
+    "Host": "api.vircities.com",
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:46.0) Gecko/20100101 Firefox/46.0",
+    "Accept" "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+    "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
+    "Accept-Encoding": "gzip, deflate",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Referer": "http://api.vircities.com/app/index.html",
+    "Cookie": "__cfduid=d7b5ba60494c6181833bb40b490dbaec81471341394; _ym_uid=147134139668239723; _ga=GA1.2.842748387.1471341396; __utma=94353562.842748387.1471341396.1471436959.1472463708.9; __utmz=94353562.1471360804.6.5.utmcsr=yandex|utmccn=(organic)|utmcmd=organic; _ga=GA1.1.842748387.1471341396; CAKEPHP=ajca838mk5fhf6uqfc4aqgbdq6; CakeCookie[lang]=rus; __utmc=94353562; _gat=1"
+}
+
+POST_HEADERS = {
+    "Host": "api.vircities.com",
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:46.0) Gecko/20100101 Firefox/46.0",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
+    "Accept-Encoding": "gzip, deflate",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Referer: http": "//api.vircities.com/app/index.html",
+    "Cookie:": "__cfduid=d7b5ba60494c6181833bb40b490dbaec81471341394; _ym_uid=147134139668239723; _ga=GA1.2.842748387.1471341396; __utma=94353562.842748387.1471341396.1471436959.1472463708.9; __utmz=94353562.1471360804.6.5.utmcsr=yandex|utmccn=(organic)|utmcmd=organic; _ga=GA1.1.842748387.1471341396; CAKEPHP=ajca838mk5fhf6uqfc4aqgbdq6; CakeCookie[lang]=rus; __utmc=94353562; _gat=1",
+    "Connection": "keep-alive",
+}
