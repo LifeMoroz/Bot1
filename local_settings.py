ddc19afd25897a85baa0fa70ab9460140bfe37f2
lifemoroz <Miromax51>
2016-09-08 14:52:27 +0300

    Gg

---
+++ local_settings.py
@@ -0,0 +1,15 @@
+SIMULATE = False
+
+HEADERS = {
+    "Host": "api.vircities.com",
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate",
+    "Referer": "http://vircities.com/",
+    "Cookie": "__cfduid=db5fec56bdbc4e358ec92330e8661b5a41473326875; _ym_uid=1473326876583013920; __utma=94353562.2110759915.1473326877.1473326877.1473326877.1; __utmb=94353562.1.10.1473326877; __utmc=94353562; __utmz=94353562.1473326877.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); __utmt=1; _ym_isad=2; _ga=GA1.2.2110759915.1473326877; _gat=1; _ga=GA1.1.2110759915.1473326877; CAKEPHP=0alussq4qoosr6fsdv5oah3126; CakeCookie[lang]=rus",
+}
+
+POST_HEADERS = HEADERS
+
+NOT_MORE_THAN = 170
